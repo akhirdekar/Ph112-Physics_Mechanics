2f6a23dc975b5acfc7a0eacca8981fc6ddea1fba
akhirdekar <144244345+akhirdekar@users.noreply.github.com>
2024-03-17 10:23:44 -0400

    CopCarProblem.py

--- CopCarProblem.py
+++ CopCarProblem.py
@@ -1,27 +1,15 @@
 import matplotlib.pyplot as plt
-
 import numpy as np
-
-
-
-
 
 # --------------- Plotting Prob 2.4c ----------------------
 
-
-
 t = np.linspace(0, 5, 200)
-
 x = t
-
 xpolice = (t*t)
 
 
-
 fig, ax = plt.subplots()
-
 plt.plot(t, x, color='red', label = 'Car')
-
 plt.plot(t, xpolice, color='blue', label = 'Police Car')
 
 #plt.xlim([0, 3])
@@ -29,13 +17,8 @@ plt.plot(t, xpolice, color='blue', label = 'Police Car')
 #plt.ylim([0, 0.6])
 
 ax.set(xlabel='time(sec)', ylabel='Position(m)')
-
 ax.grid()
-
 plt.legend(loc = 'upper left')
-
 ax.set(title='Your Title')
-
 plt.savefig('Lastname Firstname.pdf')
-
 plt.show()
